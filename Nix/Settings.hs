a6105eeaa8831bbd88fe4a0b85ad1d0aa7949a11
Shea Levy <shea@shealevy.com>
2014-07-09 23:25:21 -0400

    Remove RemoteStore and supporting code for now

--- Nix/Settings.hs
+++
@@ -1,9 +0,0 @@
-module Nix.Settings
-    ( -- * The @Settings@ type
-      Settings(..)
-    ) where
-
--- | Settings to control various aspects of the behavior of nix.
-data Settings = Settings
-    { daemonSocketFile :: !FilePath  -- ^ The path to the nix daemon socket.
-    }
