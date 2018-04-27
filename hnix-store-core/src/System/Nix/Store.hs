8b7a3670219f0e89a3d7e1740cb0c4aacc93d0f2
Shea Levy <shea@shealevy.com>
2018-04-26 21:33:06 -0700

    Add store path type.

---
+++ hnix-store-core/src/System/Nix/Store.hs
@@ -0,0 +1,41 @@
+{-|
+Description : Types and effects for interacting with the Nix store.
+Maintainer  : Shea Levy <shea@shealevy.com>
+-}
+{-# LANGUAGE DataKinds #-}
+module System.Nix.Store
+  ( PathName, pathNameContents, pathName
+  , PathHashAlgo, Path(..)
+  ) where
+
+import Crypto.Hash (Digest)
+import Crypto.Hash.Truncated (Truncated)
+import Crypto.Hash.Algorithms (SHA256)
+import Data.Text (Text)
+import Text.Regex.Base.RegexLike (makeRegex, matchTest)
+import Text.Regex.TDFA.Text (Regex)
+
+-- | The name portion of a Nix path.
+--
+-- Must be composed of a-z, A-Z, 0-9, +, -, ., _, ?, and =, can't
+-- start with a ., and must have at least one character.
+newtype PathName = PathName
+  { pathNameContents :: Text -- ^ The contents of the path name
+  }
+
+-- | A regular expression for matching a valid 'PathName'
+nameRegex :: Regex
+nameRegex =
+  makeRegex "[a-zA-Z0-9\\+\\-\\_\\?\\=][a-zA-Z0-9\\+\\-\\.\\_\\?\\=]*"
+
+-- | Construct a 'PathName', assuming the provided contents are valid.
+pathName :: Text -> Maybe PathName
+pathName n = case matchTest nameRegex n of
+  True -> Just $ PathName n
+  False -> Nothing
+
+-- | The hash algorithm used for store path hashes.
+type PathHashAlgo = Truncated SHA256 20
+
+-- | A path in a store.
+data Path = Path !(Digest PathHashAlgo) !PathName
