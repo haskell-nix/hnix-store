440dfebc288eee184925292a740758e238b6d2ef
Shea Levy <shea@shealevy.com>
2019-03-09 18:32:01 -0500

    Build expressions.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  packages = [ "hnix-store-core" "hnix-store-remote" ];
+  inherit (import ./. {}) pkgs haskellPackages;
+  hslib = pkgs.haskell.lib;
+  extract-external-inputs = p:
+    builtins.filter (dep: !(builtins.elem dep packages))
+      (map (x: x.pname) (hslib.getHaskellBuildInputs haskellPackages.${p}));
+  external-inputs = map (x: haskellPackages.${x}) (builtins.concatLists
+    (map extract-external-inputs packages));
+  package-envs = builtins.listToAttrs (map (p: {
+    name = p;
+    value = haskellPackages.${p}.env;
+  }) packages);
+in (haskellPackages.mkDerivation {
+  pname = "hnix-store-core";
+  version = "0.0.0.0";
+  libraryHaskellDepends = external-inputs;
+  license = pkgs.stdenv.lib.licenses.asl20;
+}).env // package-envs
