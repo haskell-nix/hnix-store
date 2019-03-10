7b918a929a858f012055e1def4b5a57eb3738168
Shea Levy <shea@shealevy.com>
2019-03-10 18:39:20 -0400

    System.Nix.Hash: Remove unused pragmas and cleanup.

--- hnix-store-core/src/System/Nix/Hash.hs
+++ hnix-store-core/src/System/Nix/Hash.hs
@@ -1,15 +1,6 @@
 {-|
 Description : Cryptographic hashes for hnix-store.
-Maintainer  : Shea Levy <shea@shealevy.com>; Greg Hale <imalsogreg@gmail.com>
 -}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE CPP #-}
 module System.Nix.Hash (
     HNix.Digest
 
@@ -24,4 +15,3 @@ module System.Nix.Hash (
   ) where
 
 import qualified System.Nix.Internal.Hash as HNix
-
