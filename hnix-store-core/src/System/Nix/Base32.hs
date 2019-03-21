8d1e39acb01f08ebe02fef7e5b949554b5f25c4a
Shea Levy <shea@shealevy.com>
2019-03-20 21:37:04 -0400

    Base32: Lift some helpers up for clarity

--- hnix-store-core/src/System/Nix/Base32.hs
+++ hnix-store-core/src/System/Nix/Base32.hs
@@ -18,14 +18,19 @@ encode c = T.pack $ concatMap char32 [nChar - 1, nChar - 2 .. 0]
     -- d58a11e019813902b6c4547ca61a127938b2cc20.
     nChar = fromIntegral $ ((BS.length c * 8 - 1) `div` 5) + 1
 
+    byte = BS.index c . fromIntegral
+
+    -- May need to switch to a more efficient calculation at some
+    -- point.
+    bAsInteger :: Integer
+    bAsInteger = sum [fromIntegral (byte j) * (256 ^ j)
+                     | j <- [0 .. BS.length c - 1]
+                     ]
+
     char32 :: Integer -> [Char]
     char32 i = [digits32 V.! digitInd]
       where
-        byte j   = BS.index c (fromIntegral j)
-        fromIntegral' :: Num b => Integer -> b
-        fromIntegral' = fromIntegral
-        digitInd = fromIntegral' $
-                   sum [fromIntegral (byte j) * (256^j)
-                       | j <- [0 .. BS.length c - 1]]
+        digitInd = fromIntegral $
+                   bAsInteger
                    `div` (32^i)
                    `mod` 32
