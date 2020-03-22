a4da184bd12e8136da8c96c9f24e29169a380882
Greg Hale <imalsogreg@gmail.com>
2020-03-22 14:06:16 -0400

    Use constant-space encoding and decoding for NARs

---
+++ hnix-store-core/src/System/Nix/Internal/Nar/Effects.hs
@@ -0,0 +1,75 @@
+-- |
+
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes     #-}
+
+module System.Nix.Internal.Nar.Effects
+  ( NarEffects(..)
+  , narEffectsIO
+  ) where
+
+import qualified Control.Monad.IO.Class as IO
+import qualified Data.ByteString        as BS
+import qualified Data.ByteString.Lazy   as BSL
+import           Data.Int               (Int64)
+import qualified System.Directory       as Directory
+import qualified System.IO              as IO
+import           System.Posix.Files     (createSymbolicLink, fileSize,
+                                         getFileStatus, isDirectory,
+                                         readSymbolicLink)
+
+data NarEffects (m :: * -> *) = NarEffects {
+    narReadFile   :: FilePath -> m BSL.ByteString
+  , narWriteFile  :: FilePath -> BSL.ByteString -> m ()
+  , narStreamFile :: FilePath -> m (Maybe BS.ByteString) -> m ()
+  , narListDir    :: FilePath -> m [FilePath]
+  , narCreateDir  :: FilePath -> m ()
+  , narCreateLink :: FilePath -> FilePath -> m ()
+  , narGetPerms   :: FilePath -> m Directory.Permissions
+  , narSetPerms   :: FilePath -> Directory.Permissions ->  m ()
+  , narIsDir      :: FilePath -> m Bool
+  , narIsSymLink  :: FilePath -> m Bool
+  , narFileSize   :: FilePath -> m Int64
+  , narReadLink   :: FilePath -> m FilePath
+}
+
+
+-- | A particular @NarEffects@ that uses regular POSIX for file manipulation
+--   You would replace this with your own @NarEffects@ if you wanted a
+--   different backend
+narEffectsIO :: IO.MonadIO m => NarEffects m
+narEffectsIO = NarEffects {
+    narReadFile   = IO.liftIO . BSL.readFile
+  , narWriteFile  = \a b -> IO.liftIO $ BSL.writeFile a b
+  , narStreamFile = streamStringOutIO'
+  , narListDir    = IO.liftIO . Directory.listDirectory
+  , narCreateDir  = IO.liftIO . Directory.createDirectory
+  , narCreateLink = \f t -> IO.liftIO $ createSymbolicLink f t
+  , narGetPerms   = IO.liftIO . Directory.getPermissions
+  , narSetPerms   = \f p -> IO.liftIO $ Directory.setPermissions f p
+  , narIsDir      = \d -> fmap isDirectory $ IO.liftIO (getFileStatus d)
+  , narIsSymLink  = IO.liftIO . Directory.pathIsSymbolicLink
+  , narFileSize   = \n -> fmap (fromIntegral . fileSize) $ IO.liftIO (getFileStatus n)
+  , narReadLink   = IO.liftIO . readSymbolicLink
+  }
+
+
+-- | This default implementation for @narStreamFile@ requires @IO.MonadIO@
+streamStringOutIO'
+  :: forall m.IO.MonadIO m
+  => FilePath
+  -> m (Maybe BS.ByteString)
+  -> m ()
+streamStringOutIO' f getChunk = do
+  h <- IO.liftIO $ do
+    IO.openFile f IO.WriteMode
+  let
+    go = do
+      chunk <- getChunk
+      case chunk of
+        Nothing -> IO.liftIO $ IO.hClose h
+        Just c  -> do
+          IO.liftIO $ BS.hPut h c
+          go
+
+  go
--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,8 @@
-huper: helf: {
+hlib: helf: huper: {
   hnix-store-core =
     helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
   hnix-store-remote =
     helf.callCabal2nix "hnix-store-remote" ./hnix-store-remote {};
+  algebraic-graphs =
+    hlib.dontCheck huper.algebraic-graphs;
 }
