3f95b84acc5e98865d98817b8b0aa73b7750c5f9
Richard Marko <srk@48.io>
2020-04-30 05:52:52 +0200

    Delete references to release.nix from default.nix (#7)

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 # You can build this repository using Nix by running:
 #
-#     $ nix-build -A nix-derivation release.nix
+#     $ nix-build
 #
 # You can also open up this repository inside of a Nix shell by running:
 #
-#     $ nix-shell -A nix-derivation.env release.nix
+#     $ nix-shell
 #
 # ... and then Nix will supply the correct Haskell development environment for
 # you
