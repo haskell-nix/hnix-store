dff752750d2fd324454f6f7bbb156266b66e5a78
Richard Marko <srk@48.io>
2020-07-14 13:20:04 +0200

    remote: Put tests behind io-testsuite flag
    Disabled by default since it requires `nix-daemon` binary
    and Linux namespaces support.
    
    For development this can be enabled by
    
    ```
    cabal configure --flag=io-testsuite
    ```
    
    or by adding
    
    ```
    flags: +io-testsuite
    ```
    
    to `cabal.project.local`
    
    Enabled by `callCabal2nixWithOptions` in `overlay.nix` so
    it is tested by `nix-build` and `nix-shell` brings all
    test dependencies.
    
    This is fine on NixOS where `build-tool-depends: nix:nix-daemon` works
    and we have namespaces supported.

--- overlay.nix
+++ overlay.nix
@@ -2,5 +2,5 @@ huper: helf: {
   hnix-store-core =
     helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
   hnix-store-remote =
-    helf.callCabal2nix "hnix-store-remote" ./hnix-store-remote {};
+    helf.callCabal2nixWithOptions "hnix-store-remote" ./hnix-store-remote "-fio-testsuite" {};
 }
