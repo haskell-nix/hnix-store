f325cdb7ca769a55b236250d639430555b60d581
Anton-Latukha <anton.latukha@gmail.com>
2021-01-15 02:33:00 +0200

    Core, Remote: tasty-discover dep: rm from Cabal desc, add to Nix drv
    `tasty-discover` is a run-time executable dependency, it does not belong in the
    package description.

--- overlay.nix
+++ overlay.nix
@@ -1,15 +1,17 @@
 pkgs: hlib: helf: huper: {
   hnix-store-core =
-    helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
+    ( helf.callCabal2nix "hnix-store-core" ./hnix-store-core {}
+    ).overrideAttrs (attrs: {
+        buildInputs = attrs.buildInputs ++ [
+          pkgs.haskellPackages.tasty-discover
+        ];
+    });
   hnix-store-remote =
-    (helf.callCabal2nixWithOptions
-      "hnix-store-remote"
-      ./hnix-store-remote
-      "-fio-testsuite"
-      {}
+    ( helf.callCabal2nixWithOptions "hnix-store-remote" ./hnix-store-remote "-fio-testsuite" {}
     ).overrideAttrs (attrs: {
         buildInputs = attrs.buildInputs ++ [
           pkgs.nix
+          pkgs.haskellPackages.tasty-discover
         ];
     });
 }
