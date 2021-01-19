c6e3af3ea85c79243162068c7f47c3353db0a3cb
Anton-Latukha <anton.latukha@gmail.com>
2021-01-19 12:56:16 +0200

    Cabal: declare tasty-discover a test build-tool-depend'ency
    This properly fixes the use of the tasty-discover,
    the report & info were sent upstream to fix it in docs.

--- overlay.nix
+++ overlay.nix
@@ -1,17 +1,11 @@
 pkgs: hlib: helf: huper: {
   hnix-store-core =
-    ( helf.callCabal2nix "hnix-store-core" ./hnix-store-core {}
-    ).overrideAttrs (attrs: {
-        buildInputs = attrs.buildInputs ++ [
-          pkgs.haskellPackages.tasty-discover
-        ];
-    });
+    helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
   hnix-store-remote =
     ( helf.callCabal2nixWithOptions "hnix-store-remote" ./hnix-store-remote "-fio-testsuite" {}
     ).overrideAttrs (attrs: {
         buildInputs = attrs.buildInputs ++ [
           pkgs.nix
-          pkgs.haskellPackages.tasty-discover
         ];
     });
 }
