3a169c762bd5745c374bc053f6d10b2e2edc6ed0
Anton-Latukha <anton.latukha@gmail.com>
2021-02-01 20:21:27 +0200

    Core: Internal.Nar.Streamer: m cosmetics

--- hnix-store-core/src/System/Nix/Internal/Nar/Streamer.hs
+++ hnix-store-core/src/System/Nix/Internal/Nar/Streamer.hs
@@ -10,9 +10,10 @@ module System.Nix.Internal.Nar.Streamer where
 import           Control.Monad                   (forM_, when)
 import qualified Control.Monad.IO.Class          as IO
 import           Data.Bool                       (bool)
-import qualified Data.ByteString                 as BS
-import qualified Data.ByteString.Char8           as BSC
-import qualified Data.ByteString.Lazy            as BSL
+import           Data.ByteString                 (ByteString)
+import qualified Data.ByteString                 as Bytes
+import qualified Data.ByteString.Char8           as Bytes.Char8
+import qualified Data.ByteString.Lazy            as Bytes.Lazy
 import qualified Data.List                       as List
 import qualified Data.Serialize                  as Serial
 import           GHC.Int                         (Int64)
@@ -27,69 +28,70 @@ import qualified System.Nix.Internal.Nar.Effects as Nar
 --   it while traversing the filesystem object to Nar encode
 streamNarIO
   :: forall m.(IO.MonadIO m)
-  => (BS.ByteString -> m ())
+  => (ByteString -> m ())
   -> Nar.NarEffects IO
   -> FilePath
   -> m ()
 streamNarIO yield effs basePath = do
-  yield (str "nix-archive-1")
-  parens (go basePath)
-  where
+  yield $ str "nix-archive-1"
+  parens $ go basePath
 
-    go :: FilePath -> m ()
-    go path = do
-      isDir     <- IO.liftIO $ Nar.narIsDir effs path
-      isSymLink <- IO.liftIO $ Nar.narIsSymLink effs path
-      let isRegular = not (isDir || isSymLink)
+ where
 
-      when isSymLink $ do
-        target <- IO.liftIO $ Nar.narReadLink effs path
-        yield $
-          strs ["type", "symlink", "target", BSC.pack target]
+  go :: FilePath -> m ()
+  go path = do
+    isDir     <- IO.liftIO $ Nar.narIsDir effs path
+    isSymLink <- IO.liftIO $ Nar.narIsSymLink effs path
+    let isRegular = not $ isDir || isSymLink
 
-      when isRegular $ do
-        isExec <- IO.liftIO $ isExecutable effs path
-        yield $ strs ["type","regular"]
-        when (isExec == Executable) (yield $ strs ["executable", ""])
-        fSize <- IO.liftIO $ Nar.narFileSize effs path
-        yield $ str "contents"
-        yield $ int fSize
-        yieldFile path fSize
+    when isSymLink $ do
+      target <- IO.liftIO $ Nar.narReadLink effs path
+      yield $
+        strs ["type", "symlink", "target", Bytes.Char8.pack target]
 
-      when isDir $ do
-        fs <- IO.liftIO (Nar.narListDir effs path)
-        yield $ strs ["type", "directory"]
-        forM_ (List.sort fs) $ \f -> do
-          yield $ str "entry"
-          parens $ do
-            let fullName = path </> f
-            yield (strs ["name", BSC.pack f, "node"])
-            parens (go fullName)
+    when isRegular $ do
+      isExec <- IO.liftIO $ isExecutable effs path
+      yield $ strs ["type","regular"]
+      when (isExec == Executable) $ yield $ strs ["executable", ""]
+      fSize <- IO.liftIO $ Nar.narFileSize effs path
+      yield $ str "contents"
+      yield $ int fSize
+      yieldFile path fSize
 
-    str :: BS.ByteString -> BS.ByteString
-    str t = let len =  BS.length t
-            in  int len <> padBS len t
+    when isDir $ do
+      fs <- IO.liftIO (Nar.narListDir effs path)
+      yield $ strs ["type", "directory"]
+      forM_ (List.sort fs) $ \f -> do
+        yield $ str "entry"
+        parens $ do
+          let fullName = path </> f
+          yield $ strs ["name", Bytes.Char8.pack f, "node"]
+          parens $ go fullName
 
-    padBS :: Int -> BS.ByteString -> BS.ByteString
-    padBS strSize bs = bs <> BS.replicate (padLen strSize) 0
+  str :: ByteString -> ByteString
+  str t = let len =  Bytes.length t
+          in  int len <> padBS len t
 
-    parens act = do
-      yield (str "(")
-      r <- act
-      yield (str ")")
-      return r
+  padBS :: Int -> ByteString -> ByteString
+  padBS strSize bs = bs <> Bytes.replicate (padLen strSize) 0
 
-    -- Read, yield, and pad the file
-    yieldFile :: FilePath -> Int64 -> m ()
-    yieldFile path fsize = do
-      mapM_ yield . BSL.toChunks =<< IO.liftIO (BSL.readFile path)
-      yield (BS.replicate (padLen (fromIntegral fsize)) 0)
+  parens act = do
+    yield $ str "("
+    r <- act
+    yield $ str ")"
+    return r
 
-    strs :: [BS.ByteString] -> BS.ByteString
-    strs xs = BS.concat $ str <$> xs
+  -- Read, yield, and pad the file
+  yieldFile :: FilePath -> Int64 -> m ()
+  yieldFile path fsize = do
+    mapM_ yield . Bytes.Lazy.toChunks =<< IO.liftIO (Bytes.Lazy.readFile path)
+    yield $ Bytes.replicate (padLen $ fromIntegral fsize) 0
 
-    int :: Integral a => a -> BS.ByteString
-    int n = Serial.runPut $ Serial.putInt64le (fromIntegral n)
+  strs :: [ByteString] -> ByteString
+  strs xs = Bytes.concat $ str <$> xs
+
+  int :: Integral a => a -> ByteString
+  int n = Serial.runPut $ Serial.putInt64le $ fromIntegral n
 
 
 data IsExecutable = NonExecutable | Executable
