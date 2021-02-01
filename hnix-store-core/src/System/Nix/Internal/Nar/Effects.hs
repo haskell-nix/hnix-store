8bf6f26f6b2d4ec3166cb1f9093f0f3d5924a641
Anton-Latukha <anton.latukha@gmail.com>
2021-02-01 19:24:05 +0200

    Core: Internal.Nar.Effects: m cosmetics

--- hnix-store-core/src/System/Nix/Internal/Nar/Effects.hs
+++ hnix-store-core/src/System/Nix/Internal/Nar/Effects.hs
@@ -8,23 +8,26 @@ module System.Nix.Internal.Nar.Effects
   , narEffectsIO
   ) where
 
-import qualified Control.Exception.Lifted    as Lifted
-import qualified Control.Monad.Fail          as MonadFail
-import qualified Control.Monad.IO.Class      as IO
-import           Control.Monad.Trans.Control (MonadBaseControl)
-import qualified Data.ByteString             as BS
-import qualified Data.ByteString.Lazy        as BSL
+import qualified Data.ByteString             as Bytes
+import qualified Data.ByteString.Lazy        as Bytes.Lazy
 import           Data.Int                    (Int64)
 import qualified System.Directory            as Directory
+import           System.Posix.Files          ( createSymbolicLink
+                                        , fileSize
+                                        , getFileStatus
+                                        , isDirectory
+                                        , readSymbolicLink
+                                        )
 import qualified System.IO                   as IO
-import           System.Posix.Files          (createSymbolicLink, fileSize,
-                                              getFileStatus, isDirectory,
-                                              readSymbolicLink)
+import qualified Control.Monad.IO.Class      as IO
+import           Control.Monad.Trans.Control (MonadBaseControl)
+import qualified Control.Exception.Lifted    as Exception.Lifted
+import qualified Control.Monad.Fail          as MonadFail
 
 data NarEffects (m :: * -> *) = NarEffects {
-    narReadFile   :: FilePath -> m BSL.ByteString
-  , narWriteFile  :: FilePath -> BSL.ByteString -> m ()
-  , narStreamFile :: FilePath -> m (Maybe BS.ByteString) -> m ()
+    narReadFile   :: FilePath -> m Bytes.Lazy.ByteString
+  , narWriteFile  :: FilePath -> Bytes.Lazy.ByteString -> m ()
+  , narStreamFile :: FilePath -> m (Maybe Bytes.ByteString) -> m ()
   , narListDir    :: FilePath -> m [FilePath]
   , narCreateDir  :: FilePath -> m ()
   , narCreateLink :: FilePath -> FilePath -> m ()
@@ -48,17 +51,17 @@ narEffectsIO
       MonadBaseControl IO m
      ) => NarEffects m
 narEffectsIO = NarEffects {
-    narReadFile   = IO.liftIO . BSL.readFile
-  , narWriteFile  = \a b -> IO.liftIO $ BSL.writeFile a b
+    narReadFile   = IO.liftIO . Bytes.Lazy.readFile
+  , narWriteFile  = \a -> IO.liftIO . Bytes.Lazy.writeFile a
   , narStreamFile = streamStringOutIO
   , narListDir    = IO.liftIO . Directory.listDirectory
   , narCreateDir  = IO.liftIO . Directory.createDirectory
-  , narCreateLink = \f t -> IO.liftIO $ createSymbolicLink f t
+  , narCreateLink = \f -> IO.liftIO . createSymbolicLink f
   , narGetPerms   = IO.liftIO . Directory.getPermissions
-  , narSetPerms   = \f p -> IO.liftIO $ Directory.setPermissions f p
-  , narIsDir      = \d -> fmap isDirectory $ IO.liftIO (getFileStatus d)
+  , narSetPerms   = \f -> IO.liftIO . Directory.setPermissions f
+  , narIsDir      = fmap isDirectory . IO.liftIO . getFileStatus
   , narIsSymLink  = IO.liftIO . Directory.pathIsSymbolicLink
-  , narFileSize   = \n -> fmap (fromIntegral . fileSize) $ IO.liftIO (getFileStatus n)
+  , narFileSize   = fmap (fromIntegral . fileSize) . IO.liftIO . getFileStatus
   , narReadLink   = IO.liftIO . readSymbolicLink
   , narDeleteDir  = IO.liftIO . Directory.removeDirectoryRecursive
   , narDeleteFile = IO.liftIO . Directory.removeFile
@@ -72,23 +75,25 @@ streamStringOutIO
     MonadFail.MonadFail m,
     MonadBaseControl IO m
   ) => FilePath
-  -> m (Maybe BS.ByteString)
+  -> m (Maybe Bytes.ByteString)
   -> m ()
 streamStringOutIO f getChunk =
-  Lifted.bracket
-    (IO.liftIO (IO.openFile f IO.WriteMode)) (IO.liftIO . IO.hClose) go
-  `Lifted.catch`
+  Exception.Lifted.bracket
+    (IO.liftIO $ IO.openFile f IO.WriteMode)
+    (IO.liftIO . IO.hClose)
+    go
+  `Exception.Lifted.catch`
     cleanupException
-  where
-    go :: IO.Handle -> m ()
-    go handle = do
-      chunk <- getChunk
-      case chunk of
-        Nothing -> return ()
-        Just c  -> do
-          IO.liftIO $ BS.hPut handle c
-          go handle
-    cleanupException (e :: Lifted.SomeException) = do
-      IO.liftIO $ Directory.removeFile f
-      MonadFail.fail $
-        "Failed to stream string to " ++ f ++ ": " ++ show e
+ where
+  go :: IO.Handle -> m ()
+  go handle = do
+    chunk <- getChunk
+    case chunk of
+      Nothing -> return ()
+      Just c  -> do
+        IO.liftIO $ Bytes.hPut handle c
+        go handle
+  cleanupException (e :: Exception.Lifted.SomeException) = do
+    IO.liftIO $ Directory.removeFile f
+    MonadFail.fail $
+      "Failed to stream string to " <> f <> ": " <> show e
