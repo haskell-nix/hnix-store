35da3c9ae1c8e9845e29b74b304d62e9df7a44d2
Anton-Latukha <anton.latukha@gmail.com>
2021-06-10 12:57:54 +0300

    overlay.nix: stop loading Core from master for Remote

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,4 @@
 pkgs: hlib: helf: huper: {
-  hnix-store-core =
-    helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
   hnix-store-remote =
     ( helf.callCabal2nixWithOptions "hnix-store-remote" ./hnix-store-remote "-fio-testsuite" {}
     ).overrideAttrs (attrs: {
