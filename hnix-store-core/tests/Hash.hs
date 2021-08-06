2bf02a92d8b033658549ed32c30466b7e08fcf1c
Anton-Latukha <anton.latukha@gmail.com>
2021-08-07 00:49:01 +0300

    Core: tests: Hash: spec_nixhash: refactor

--- hnix-store-core/tests/Hash.hs
+++ hnix-store-core/tests/Hash.hs
@@ -4,7 +4,6 @@
 
 module Hash where
 
-import qualified Data.ByteString.Char8       as BSC
 import qualified Data.ByteString.Base16      as B16
 import qualified System.Nix.Base32           as B32
 import qualified Data.ByteString.Base64.Lazy as B64
@@ -49,7 +48,7 @@ spec_hash = do
 -- | Test that Nix-like base32 encoding roundtrips
 prop_nixBase32Roundtrip :: Property
 prop_nixBase32Roundtrip = forAllShrink nonEmptyString genericShrink $
-  \x -> pure (BSC.pack x) === (B32.decode . B32.encode . BSC.pack $ x)
+  \x -> pure (encodeUtf8 x) === (B32.decode . B32.encode . encodeUtf8 $ x)
 
 -- | API variants
 prop_nixBase16Roundtrip :: StorePathHashPart -> Property
@@ -62,48 +61,68 @@ spec_nixhash = do
 
   describe "hashing parity with nix-nash" $ do
 
-    let
-      samples = [
-          ( "800d59cfcd3c05e900cb4e214be48f6b886a08df"
-          , "vw46m23bizj4n8afrc0fj19wrp7mj3c0"
-          , "gA1Zz808BekAy04hS+SPa4hqCN8="
-          )
-        , ( "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
-          , "1b8m03r63zqhnjf7l5wnldhh7c134ap5vpj0850ymkq1iyzicy5s"
-          , "ungWv48Bz+pBQUDeXa4iI7ADYaOWF3qctBD/YfIAFa0="
-          )
-        , ( "204a8fc6dda82f0a0ced7beb8e08a41657c16ef468b228a8279be331a703c33596fd15c13b1b07f9aa1d3bea57789ca031ad85c7a71dd70354ec631238ca3445"
-          , "12k9jiq29iyqm03swfsgiw5mlqs173qazm3n7daz43infy12pyrcdf30fkk3qwv4yl2ick8yipc2mqnlh48xsvvxl60lbx8vp38yji0"
-          , "IEqPxt2oLwoM7XvrjgikFlfBbvRosiioJ5vjMacDwzWW/RXBOxsH+aodO+pXeJygMa2Fx6cd1wNU7GMSOMo0RQ=="
-          )
-        ]
+    cmp
+      "b16 encoded . b32 decoded should equal original b16"
+      B16.encode B32.decode b32s b16s
 
-    it "b16 encoded . b32 decoded should equal original b16" $
-      forM_ samples $ \(b16, b32, _b64) -> shouldBe (B16.encode <$> B32.decode b32) (pure b16)
+    cmp
+      "b64 encoded . b32 decoded should equal original b64"
+      (B64.encode . fromStrict) B32.decode b32s b64s
 
-    it "b64 encoded . b32 decoded should equal original b64" $
-      forM_ samples $ \(_b16, b32, b64) -> shouldBe (B64.encode . fromStrict <$> B32.decode b32) (pure b64)
+    cmp
+      "b32 encoded . b64 decoded should equal original b32"
+      (B32.encode . toStrict) B64.decode b64s b32s
 
-    it "b32 encoded . b64 decoded should equal original b32" $
-      forM_ samples $ \(_b16, b32, b64) -> shouldBe (B32.encode . toStrict <$> B64.decode b64 ) (pure b32)
+    cmp
+      "b16 encoded . b64 decoded should equal original b16"
+      (B16.encode . toStrict) B64.decode b64s b16s
 
-    it "b16 encoded . b64 decoded should equal original b16" $
-      forM_ samples $ \(b16, _b32, b64) -> shouldBe (B16.encode . toStrict <$> B64.decode b64 ) (pure b16)
-
-    it "b32 encoded . b16 decoded should equal original b32" $
-      forM_ samples $ \(b16, b32, _b64) -> shouldBe (B32.encode
 #if MIN_VERSION_base16_bytestring(1,0,0)
-        <$> B16.decode b16) (pure b32)
-#else
-        $ fst $ B16.decode b16) (b32)
+    cmp
+      "b32 encoded . b16 decoded should equal original b32"
+      B32.encode B16.decode b16s b32s
 
-#endif
+    cmp
+      "b64 encoded . b16 decoded should equal original b64"
+      (B64.encode . fromStrict) B16.decode b16s b64s
+#else
+    it "b32 encoded . b16 decoded should equal original b32" $
+      traverse_ (\ b -> shouldBe (B32.encode $ fst $ B16.decode $ fst b) (snd b)) $ zip b16s b32s
 
     it "b64 encoded . b16 decoded should equal original b64" $
-      forM_ samples $ \(b16, _b32, b64) -> shouldBe (B64.encode . fromStrict
-#if MIN_VERSION_base16_bytestring(1,0,0)
-        <$> B16.decode b16) (pure b64)
-#else
-        $ fst $ B16.decode b16 ) (b64)
+      traverse_ (\ b -> shouldBe (B64.encode . fromStrict $ fst $ B16.decode $ fst b) (snd b)) $ zip b16s b64s
 #endif
 
+ where
+  cmp
+    :: ( Eq b
+       , Show b
+       )
+    => String
+    -> (a -> b)
+    -> (c -> Either String a)
+    -> [c]
+    -> [b]
+    -> SpecWith ()
+  cmp s f1 f2 b1 b2 = it s $ traverse_ (uncurry shouldBe . bimap (fmap f1 . f2) pure) $ zip b1 b2
+
+  b16s = takeAxis (\(a,_,_) -> a)
+  b32s = takeAxis (\(_,b,_) -> b)
+  b64s = takeAxis (\(_,_,c) -> c)
+
+  takeAxis f = fmap f samples
+
+  samples =
+    [ ( "800d59cfcd3c05e900cb4e214be48f6b886a08df"
+      , "vw46m23bizj4n8afrc0fj19wrp7mj3c0"
+      , "gA1Zz808BekAy04hS+SPa4hqCN8="
+      )
+    , ( "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
+      , "1b8m03r63zqhnjf7l5wnldhh7c134ap5vpj0850ymkq1iyzicy5s"
+      , "ungWv48Bz+pBQUDeXa4iI7ADYaOWF3qctBD/YfIAFa0="
+      )
+    , ( "204a8fc6dda82f0a0ced7beb8e08a41657c16ef468b228a8279be331a703c33596fd15c13b1b07f9aa1d3bea57789ca031ad85c7a71dd70354ec631238ca3445"
+      , "12k9jiq29iyqm03swfsgiw5mlqs173qazm3n7daz43infy12pyrcdf30fkk3qwv4yl2ick8yipc2mqnlh48xsvvxl60lbx8vp38yji0"
+      , "IEqPxt2oLwoM7XvrjgikFlfBbvRosiioJ5vjMacDwzWW/RXBOxsH+aodO+pXeJygMa2Fx6cd1wNU7GMSOMo0RQ=="
+      )
+    ]
