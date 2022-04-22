d34710a046d5431ef19698d8457463749982fbc0
Richard Marko <srk@48.io>
2022-04-22 12:49:07 +0200

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }: let
+{ rev ?  "c542baa0c894796c92a8173dead027f3b952c22e"
+, pkgs ?
+    if ((rev == "") || (rev == "default") || (rev == "local"))
+      then import <nixpkgs> {}
+      # Do not guard with hash, so the project is able to use current channels (rolling `rev`) of Nixpkgs
+      else import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz") {}
+    // {
+      # Try to build dependencies even if they are marked broken.
+      config.allowBroken = true;
+    }
+}: let
   overlay = import ./overlay.nix pkgs pkgs.haskell.lib;
   overrideHaskellPackages = orig: {
     buildHaskellPackages =
