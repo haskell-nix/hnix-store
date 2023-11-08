591f5bd3d4327ef44c4b585b9f1f1fc99ea56133
Richard Marko <srk@48.io>
2023-11-08 18:03:27 +0100

    Revert "overlay.nix: stop loading Core from master for Remote"
    This reverts commit 35da3c9ae1c8e9845e29b74b304d62e9df7a44d2.

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,6 @@
 pkgs: hlib: helf: huper: {
+  hnix-store-core =
+    helf.callCabal2nix "hnix-store-core" ./hnix-store-core {};
   hnix-store-remote =
     ( helf.callCabal2nixWithOptions "hnix-store-remote" ./hnix-store-remote "-fio-testsuite" { }
     ).overrideAttrs (attrs: {
