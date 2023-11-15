ddc914bcb482dca610d5b10c5a4164b15043f38f
Richard Marko <srk@48.io>
2023-11-15 16:02:07 +0100

    remote: add Serialize instances for BuildMode, BuildStatus, Text + tests

--- hnix-store-remote/src/System/Nix/Store/Remote/Serialize.hs
+++ hnix-store-remote/src/System/Nix/Store/Remote/Serialize.hs
@@ -1,5 +1,36 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
 {-|
-Description : Incremental decoding
+Description : Serialize instances for complex types
 Maintainer  : srk <srk@48.io>
 |-}
 module System.Nix.Store.Remote.Serialize where
+
+import Data.Text
+import Data.Serialize (Serialize(..))
+
+import System.Nix.Build (BuildMode(..), BuildStatus(..), BuildResult(..))
+import System.Nix.Store.Remote.Serialize.Prim
+
+instance Serialize Text where
+  get = getText
+  put = putText
+
+instance Serialize BuildMode where
+  get = getEnum
+  put = putEnum
+
+instance Serialize BuildStatus where
+  get = getEnum
+  put = putEnum
+
+instance Serialize BuildResult where
+  get =
+    BuildResult
+      <$> get
+      -- TODO(srk): fishy
+      <*> (Just <$> get)
+      <*> (fromIntegral <$> getInt)
+      <*> getBool
+      <*> getTime
+      <*> getTime
+  put = undefined
--- hnix-store-remote/tests/SerializeSpec.hs
+++ hnix-store-remote/tests/SerializeSpec.hs
@@ -1,20 +1,27 @@
 {-# LANGUAGE NumericUnderscores #-}
 module SerializeSpec where
 
-import Prelude hiding (putText)
+import Data.ByteString (ByteString)
 import Data.Fixed (Uni)
+import Data.HashSet (HashSet)
+import Data.Serialize (Serialize(..))
 import Data.Serialize.Get (Get, runGet)
 import Data.Serialize.Put (Putter, runPut)
-import Data.Time (NominalDiffTime, UTCTime)
+import Data.Text (Text)
+import Data.Time (NominalDiffTime)
+import Test.Hspec (Spec, describe, it, shouldBe)
 import Test.Tasty.QuickCheck
-import Test.QuickCheck.Instances
+import Test.QuickCheck.Instances ()
 
-import System.Nix.StorePath (StoreDir, StorePath)
-import System.Nix.Store.Remote.Serialize
-import System.Nix.Store.Remote.Serialize.Prim
-
+import qualified Data.Either
 import qualified Data.HashSet
 import qualified Data.Time.Clock.POSIX
+import qualified System.Nix.Build
+
+import System.Nix.Build (BuildMode, BuildStatus, BuildResult)
+import System.Nix.StorePath (StoreDir, StorePath)
+import System.Nix.Store.Remote.Serialize ()
+import System.Nix.Store.Remote.Serialize.Prim
 
 roundTrip :: (Eq a, Show a) => Putter a -> Get a -> a -> Property
 roundTrip p g a = res === Right a
@@ -72,10 +79,52 @@ prop_path :: StoreDir -> StorePath -> Property
 prop_path = \sd ->
   roundTrip
     (putPath sd)
-    (fromRight undefined <$> getPath sd)
+    (Data.Either.fromRight undefined <$> getPath sd)
 
 prop_paths :: StoreDir -> HashSet StorePath -> Property
 prop_paths = \sd ->
   roundTrip
     (putPaths sd)
-    (Data.HashSet.map (fromRight undefined) <$> getPaths sd)
+    (Data.HashSet.map (Data.Either.fromRight undefined) <$> getPaths sd)
+
+-- * Serialize
+roundTripS :: (Eq a, Serialize a, Show a) => a -> Property
+roundTripS a = res === Right a
+  where res = runGet get (runPut (put a))
+
+-- ** Text
+
+prop_Text :: Text -> Property
+prop_Text = roundTripS
+
+-- ** BuildMode
+
+prop_buildMode :: BuildMode -> Property
+prop_buildMode = roundTripS
+
+prop_buildStatus :: BuildStatus -> Property
+prop_buildStatus = roundTripS
+
+spec_buildMode :: Spec
+spec_buildMode =
+  let it' name constr value = it name $ runPut (put constr) `shouldBe` runPut (putInt value)
+  in do
+    describe "Build enum order matches Nix" $ do
+      it' "Normal" System.Nix.Build.Normal 0
+      it' "Repair" System.Nix.Build.Repair 1
+      it' "Check"  System.Nix.Build.Check  2
+
+    describe "BuildStatus enum order matches Nix" $ do
+      it' "Built"            System.Nix.Build.Built             0
+      it' "Substituted"      System.Nix.Build.Substituted       1
+      it' "AlreadyValid"     System.Nix.Build.AlreadyValid      2
+      it' "PermanentFailure" System.Nix.Build.PermanentFailure  3
+      it' "InputRejected"    System.Nix.Build.InputRejected     4
+      it' "OutputRejected"   System.Nix.Build.OutputRejected    5
+      it' "TransientFailure" System.Nix.Build.TransientFailure  6
+      it' "CachedFailure"    System.Nix.Build.CachedFailure     7
+      it' "TimedOut"         System.Nix.Build.TimedOut          8
+      it' "MiscFailure"      System.Nix.Build.MiscFailure       9
+      it' "DependencyFailed" System.Nix.Build.DependencyFailed 10
+      it' "LogLimitExceeded" System.Nix.Build.LogLimitExceeded 11
+      it' "NotDeterministic" System.Nix.Build.NotDeterministic 12
