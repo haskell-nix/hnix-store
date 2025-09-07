10fa11207e4e30d8194ade3c51ccaa8fa1cf4e10
John Ericson <John.Ericson@Obsidian.Systems>
2025-09-07 05:46:07 -0400

    WIP

--- hnix-store-tests/src/System/Nix/Arbitrary/DerivedPath.hs
+++ hnix-store-tests/src/System/Nix/Arbitrary/DerivedPath.hs
@@ -4,11 +4,10 @@
 module System.Nix.Arbitrary.DerivedPath where
 
 import Data.Set qualified
-import Test.QuickCheck (Arbitrary(..), oneof)
-import Test.QuickCheck.Arbitrary.Generic (GenericArbitrary(..))
+import Test.QuickCheck
 import System.Nix.Arbitrary.OutputName ()
 import System.Nix.Arbitrary.StorePath ()
-import System.Nix.DerivedPath (SingleDerivedPath, DerivedPath, OutputsSpec(..))
+import System.Nix.DerivedPath
 
 instance Arbitrary OutputsSpec where
   arbitrary = oneof
@@ -18,8 +17,28 @@ instance Arbitrary OutputsSpec where
       <$> ((:) <$> arbitrary <*> arbitrary)
     ]
 
-deriving via GenericArbitrary SingleDerivedPath
-  instance Arbitrary SingleDerivedPath
+arbitrarySingleDerivedPath :: Word -> Gen SingleDerivedPath
+arbitrarySingleDerivedPath = \case
+  0 -> SingleDerivedPath_Opaque <$> arbitrary
+  depth -> SingleDerivedPath_Built
+    <$> (arbitrarySingleDerivedPath $ depth - 1)
+    <*> arbitrary
 
-deriving via GenericArbitrary DerivedPath
-  instance Arbitrary DerivedPath
+arbitraryDerivedPath :: Word -> Gen DerivedPath
+arbitraryDerivedPath = \case
+  0 -> DerivedPath_Opaque <$> arbitrary
+  depth -> DerivedPath_Built
+    <$> (arbitrarySingleDerivedPath $ depth - 1)
+    <*> arbitrary
+
+instance Arbitrary SingleDerivedPath where
+  arbitrary = do
+    n <- getSize
+    k <- choose (0, n)
+    arbitrarySingleDerivedPath $ fromIntegral k
+
+instance Arbitrary DerivedPath where
+  arbitrary = do
+    n <- getSize
+    k <- choose (0, n)
+    arbitraryDerivedPath $ fromIntegral k
